c900328bc1a833d6391d1052e50caa968fe40c4c
redfumo <themillenniumfox8888@gmail.com>
2023-12-17 21:14:21 -0500

    Add informative message and link to campus

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,8 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -11,10 +13,18 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+
+      {/* Prints out message if the student is not enrolled anywhere */}
+      {student.campusId == null ? (
+        <div><h3>This student is not enrolled at any campus.</h3></div>
+      ) : (
+      <Link to={`/campus/${student.campusId}`}>
+        <h3>{student.campus.name}</h3>
+      </Link>
+      )
+      }
     </div>
   );
-
 };
 
 export default StudentView;
