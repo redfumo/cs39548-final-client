1ee4aef96d417314d0c276028751b85db9d0efca
redfumo <themillenniumfox8888@gmail.com>
2023-12-18 21:08:53 -0500

    Added image to home page

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -9,6 +9,7 @@ const HomePageView = () => {
   return (
     <div >
       <h1>Home Page</h1>
+      <img src="https://2.bp.blogspot.com/-6DIz5hSsANY/W0mGIqPg-uI/AAAAAAABNXM/MM9rJsc2wKkTMQk8L8Gqc2N1IUXWgMOJgCLcBGAs/s800/school_shiken_kaijou_boy_girl.png" width={500} alt="going to school"/>
     </div>
   );    
 }
